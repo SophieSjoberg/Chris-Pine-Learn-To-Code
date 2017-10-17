0422a1dd7d21470e8008450d0f7e2f1c58b0d63f
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-10-17 13:48:59 +0200

    added

---
+++ full_name.rb
@@ -0,0 +1,13 @@
+puts 'What\'s your first name?'
+
+firstname = gets.chomp
+
+puts 'What\'s your middle name?'
+
+middlename = gets.chomp
+
+puts 'What\'s your last name?'
+
+lastname = gets.chomp
+
+puts 'Hello ' + firstname + ' ' + middlename + ' ' + lastname
