e820d667fcaa9f13326f1e71b1e23c4b043e1a7e
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-10-17 13:37:15 +0200

    pushed

---
+++ calc.rb
@@ -0,0 +1,48 @@
+
+puts 365*24
+puts 60 * (8760*10)
+puts (60*60) * (8760*30)
+puts (1298000000 / 60) / 60
+puts (360555 / 24) /365
+
+
+puts 'I like' + ' apple pie.'
+
+puts 'blink ' * 4
+
+puts 2 * 5
+puts '2' * 5
+puts '2 * 5'
+
+
+
+
+var1 = 2
+var2 = '5'
+
+puts var1.to_s + var2
+puts var1 +var2.to_i
+
+puts '15'.to_f
+puts '99.999'.to_f
+puts '99.999'.to_i
+puts ''
+puts '5 is my favorite number!'.to_i
+puts 'Who asked you about 5 or whatever?'.to_i
+puts 'Your momma did.'.to_f
+puts ''
+puts 'stringy'.to_s
+puts 3.to_i
+
+puts 20
+puts 20.to_s
+puts '20'
+
+
+     letters = 'aAbBcCdDeE'
+puts letters.upcase
+puts letters.downcase
+puts letters.swapcase
+puts letters.capitalize
+puts ' a'.capitalize
+puts letters
